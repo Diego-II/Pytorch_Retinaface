f8be35f85f4d2fed6c752c702f225ce3f81d844b
Diego-II <43184602+dirarra@users.noreply.github.com>
2021-01-16 19:17:21 -0300

    fix import efficientnet

--- models/retinaface.py
+++ models/retinaface.py
@@ -9,7 +9,8 @@ from models.net import MobileNetV1 as MobileNetV1
 from models.net import FPN as FPN
 from models.net import SSH as SSH
 
-from efficientnet_pytorch.model import EfficientNet as EffNet
+from models.efficientnet_pytorch.model import EfficientNet as EffNet
+
 
 class ClassHead(nn.Module):
     def __init__(self,inchannels=512,num_anchors=3):
