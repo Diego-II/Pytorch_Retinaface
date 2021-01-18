54db32babbf508600294bdb88fe37dfb3cba4fc1
Diego-II <43184602+dirarra@users.noreply.github.com>
2021-01-17 23:17:37 -0300

    hotfix efficient net

--- models/retinaface.py
+++ models/retinaface.py
@@ -79,7 +79,7 @@ class RetinaFace(nn.Module):
 
         if cfg['name'] == 'efficientnet-b4':
             in_channels_list = [
-            24,  32, 1280,
+            24,  32, 56,
         ]    
         else:
             in_channels_stage2 = cfg['in_channel']
