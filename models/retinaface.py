738b855603b55e13fffabba8b2e73d330e874ec8
Diego-II <43184602+dirarra@users.noreply.github.com>
2021-01-19 02:09:33 -0300

    try con instalacion de pytorch

--- models/retinaface.py
+++ models/retinaface.py
@@ -14,11 +14,13 @@ from models.TResNet.models import create_model
 from models.TResNet.models.tresnet import TResnetM
 
 def load_tresnetm():
-    url = 'https://miil-public-eu.oss-eu-central-1.aliyuncs.com/model-zoo/tresnet/tresnet_m_448.pth'
-    weights_m = torch.hub.load_state_dict_from_url(url)
-    model_params ={ 'num_classes': 1000,'remove_aa_jit': False}
-    model = TResnetM(model_params)
-    model.load_state_dict(weights_m)
+    from torch_tresnet import tresnet_m, tresnet_m_448
+    # url = 'https://miil-public-eu.oss-eu-central-1.aliyuncs.com/model-zoo/tresnet/tresnet_m_448.pth'
+    # weights_m = torch.hub.load_state_dict_from_url(url)
+    # model_params ={ 'num_classes': 1000,'remove_aa_jit': False}
+    # model = TResnetM(model_params)
+    # model.load_state_dict(weights_m)
+    model = tresnet_m_448(pretrain=True, num_classes=10)
 
     return model
 
