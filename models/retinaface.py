7347743acb478af0214c085237fc6beb565367a2
Diego-II <43184602+dirarra@users.noreply.github.com>
2021-01-21 23:58:35 -0300

    hotfix in channels

--- models/retinaface.py
+++ models/retinaface.py
@@ -102,7 +102,7 @@ class RetinaFace(nn.Module):
             in_channels_stage2 * 8,
         ]
         if cfg['name'] == 'tresnet':
-            in_channels_list = [64, 128, 512]
+            in_channels_list = [64, 176, 512]
         
         out_channels = cfg['out_channel']
         self.fpn = FPN(in_channels_list,out_channels, cfg)
